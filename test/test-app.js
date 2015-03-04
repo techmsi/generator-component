7f21b146bc98284ae18e9db86b84b551d9878991
techmsi <Sophie.Idromenos@nymag.com>
2015-03-04 13:50:53 -0500

    Test: Modified default test case

--- test/test-app.js
+++ test/test-app.js
@@ -1,27 +1,69 @@
 'use strict';
 
-var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
-var os = require('os');
+var path = require('path'),
+ chalk = require('chalk'),
+ assert = require('yeoman-generator').assert,
+ helpers = require('yeoman-generator').test,
+ os = require('os'),
+ generatorPath = '../generators/app',
+ outputDir = './temp-test',
+ contextsDir = 'contexts',
+ componentName = 'my-test-component',
+ promptsDefault = require(path.resolve('test/mockData', 'promptsDefault.json')),
+ promptsNoMediaQueries = require(path.resolve('test/mockData', 'promptsNoMediaQueries.json')),
+ prompts2Contexts = require(path.resolve('test/mockData', 'prompts2Contexts.json')),
+ promptsNoContexts = require(path.resolve('test/mockData', 'promptsNoContexts.json'));
 
-describe('component:app', function () {
+var filesDefaults = [
+  componentName + '/' + 'index.html',
+  componentName + '/' + 'index.js',
+  componentName + '/' + 'print.css',
+  componentName + '/' + contextsDir + '/' + 'mysite/0-600.css',
+  componentName + '/' + contextsDir + '/' + 'mysite/600-1024.css',
+  componentName + '/' + contextsDir + '/' + 'mysite/1024+.css'
+];
+
+var files2Contexts = [
+  componentName + '/' + 'index.html',
+  componentName + '/' + 'index.js',
+  componentName + '/' + 'print.css',
+  componentName + '/' + contextsDir + '/' + 'mysite/0-600.css',
+  componentName + '/' + contextsDir + '/' + 'mysite/600-1024.css',
+  componentName + '/' + contextsDir + '/' + 'mysite/1024+.css',
+  componentName + '/' + contextsDir + '/' + 'myothersite/0-600.css',
+  componentName + '/' + contextsDir + '/' + 'myothersite/600-1024.css',
+  componentName + '/' + contextsDir + '/' + 'myothersite/1024+.css'
+];
+
+var filesNoContexts = [
+  componentName + '/' + 'index.html',
+  componentName + '/' + 'index.js',
+  componentName + '/' + 'print.css',
+  componentName + '/' + '0-600.css',
+  componentName + '/' + '600-1024.css',
+  componentName + '/' + '1024+.css'
+];
+
+var filesNoMediaQueries = [
+  componentName + '/' + 'index.html',
+  componentName + '/' + 'index.js',
+  componentName + '/' + 'print.css',
+  componentName + '/' + 'all.css'
+];
+
+describe('component:app - case 1', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
+    helpers.run(path.join(__dirname, generatorPath))
+      .inDir(path.join(os.tmpdir(), outputDir))
+      .withArguments([componentName])              // Mock the arguments
+      .withPrompt(promptsDefault)
+      .on('ready', function (generator) {
+        console.log('\nTesting ' + chalk.yellow(componentName) + ' component...');
       })
       .on('end', done);
   });
 
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
+  it('create files - (default)', function () {
+    assert.file(filesDefaults);
   });
 });
